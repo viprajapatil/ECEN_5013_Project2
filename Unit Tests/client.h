4e37c95973407fe0395cae0f05c6b7f4a4da3432
viprajapatil <31683311+viprajapatil@users.noreply.github.com>
2018-04-29 17:37:15 -0600

    Create client.h

---
+++ Unit Tests/client.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+
+int gas_value();
+
+int flame_value();
+
+int temp_value();
